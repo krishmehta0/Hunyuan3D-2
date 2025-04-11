681f841f90d21991dad09a8555f5aaff680cd4bc
krishmehta0 <krish@palatialxr.com>
2025-04-11 02:35:28 -0400

    setup modifications

--- hy3dgen/texgen/custom_rasterizer/setup.py
+++ hy3dgen/texgen/custom_rasterizer/setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# build custom rasterizer
-# build with `python setup.py install`
+# Build custom rasterizer
+# Build with `python setup.py install`
 # nvcc is needed
 
-custom_rasterizer_module = CUDAExtension('custom_rasterizer_kernel', [
-    'lib/custom_rasterizer_kernel/rasterizer.cpp',
-    'lib/custom_rasterizer_kernel/grid_neighbor.cpp',
-    'lib/custom_rasterizer_kernel/rasterizer_gpu.cu',
-])
+custom_rasterizer_module = CUDAExtension(
+    name='custom_rasterizer_kernel',
+    sources=[
+        'lib/custom_rasterizer_kernel/rasterizer.cpp',
+        'lib/custom_rasterizer_kernel/grid_neighbor.cpp',
+        'lib/custom_rasterizer_kernel/rasterizer_gpu.cu',
+    ],
+    include_dirs=['/usr/include/x86_64-linux-gnu'],  # Corrected path
+)
 
 setup(
     packages=find_packages(),
